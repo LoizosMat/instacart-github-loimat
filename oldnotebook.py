5d3a5efb908361582d10a9972c40cf692ec2cfac
LoizosMat <65346744+LoizosMat@users.noreply.github.com>
2020-05-30 13:27:42 +0300

    old

---
+++ oldnotebook.py
@@ -0,0 +1,555 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# For data manipulation
+import pandas as pd              
+
+# Garbage Collector to free up memory
+import gc                         
+gc.enable()                       # Activate 
+
+
+# In[2]:
+
+
+orders = pd.read_csv('../input/instacart-market-basket-analysis/orders.csv')
+order_products_train = pd.read_csv('../input/instacart-market-basket-analysis/order_products__train.csv')
+order_products_prior = pd.read_csv('../input/instacart-market-basket-analysis/order_products__prior.csv')
+products = pd.read_csv('../input/instacart-market-basket-analysis/products.csv')
+aisles = pd.read_csv('../input/instacart-market-basket-analysis/aisles.csv')
+departments = pd.read_csv('../input/instacart-market-basket-analysis/departments.csv')
+
+
+
+# In[4]:
+
+
+# We convert character variables into category. 
+# In Python, a categorical variable is called category and has a fixed number of different values
+aisles['aisle'] = aisles['aisle'].astype('category')
+departments['department'] = departments['department'].astype('category')
+orders['eval_set'] = orders['eval_set'].astype('category')
+products['product_name'] = products['product_name'].astype('category')
+
+
+# In[5]:
+
+
+#Merge the orders DF with order_products_prior by their order_id, keep only these rows with order_id that they are appear on both DFs
+op = orders.merge(order_products_prior, on='order_id', how='inner')
+op.head()
+
+
+# In[6]:
+
+
+op['order_number_back'] = op.groupby('user_id')['order_number'].transform(max) - op.order_number +1
+op5 = op[op.order_number_back <= 5]
+op5.head()
+last_five = op5.groupby(['user_id','product_id'])[['order_id']].count()
+last_five.columns = ['times_last5']
+last_five = last_five.reset_index()
+last_five.head(10)
+
+
+# In[7]:
+
+
+last5_ratio = op5.groupby(['user_id','product_id'])[['order_id']].count()
+last5_ratio.columns = ['times_last5_ratio']
+last5_ratio = last5_ratio[['times_last5_ratio']] / 5
+last5_ratio = last5_ratio.reset_index()
+last5_ratio.head()
+
+
+# In[8]:
+
+
+last_five = last_five.merge(last5_ratio, on=['user_id','product_id'], how='left')
+last_five.head()
+
+
+# In[9]:
+
+
+max_days_since_last_order_l5 = op5.groupby(['user_id','product_id'])[['days_since_prior_order']].max()
+max_days_since_last_order_l5.columns = ['max_days_since_last5'] 
+max_days_since_last_order_l5 = max_days_since_last_order_l5.reset_index() 
+max_days_since_last_order_l5.head(50)
+
+
+# In[10]:
+
+
+max_days_since_last_order_l5.head()
+
+
+# In[11]:
+
+
+max_days_since_last_order = op.groupby(['user_id','product_id'])[['days_since_prior_order']].max()
+max_days_since_last_order.columns = ['max_days_since_last'] 
+max_days_since_last_order = max_days_since_last_order.reset_index() 
+max_days_since_last_order.head(50)
+
+
+# In[12]:
+
+
+max_days_since_last_order.head(50)
+
+
+# In[13]:
+
+
+max_days_since_last_order = max_days_since_last_order.merge(max_days_since_last_order_l5, on=['user_id','product_id'], how='left')
+max_days_since_last_order.head()
+
+
+# In[14]:
+
+
+max_days_since_last_order = max_days_since_last_order.fillna(0)
+max_days_since_last_order.head()
+
+
+# In[15]:
+
+
+del [max_days_since_last_order_l5]
+
+
+# In[16]:
+
+
+last_five = last_five.merge(max_days_since_last_order, on=['user_id','product_id'], how='left')
+last_five.head()
+
+
+# In[17]:
+
+
+del [max_days_since_last_order]
+
+
+# In[18]:
+
+
+prr = op.groupby('product_id').filter(lambda x: x.shape[0] >40)
+prr = op.groupby('product_id')['reordered'].mean().to_frame('product_reordered_ratio') #
+prr.head()
+
+
+# In[19]:
+
+
+prr = prr.reset_index()
+prr.head(50)
+
+
+# In[20]:
+
+
+user = op.groupby('user_id')['order_number'].max().to_frame('u_total_orders')
+user = user.reset_index()
+user.head()
+
+
+# In[21]:
+
+
+urr = op.groupby('user_id')['reordered'].mean().to_frame('user_reordered_ratio') #
+urr = urr.reset_index()
+urr.head()
+
+
+# In[22]:
+
+
+user = user.merge(urr, on='user_id', how='left')
+
+del urr
+gc.collect()
+
+user.head() 
+
+
+# In[23]:
+
+
+uxp = op.groupby(['user_id', 'product_id'])['order_id'].count().to_frame('uxp_total_bought')
+uxp.head()
+
+
+# In[24]:
+
+
+uxp = uxp.reset_index()
+uxp.head()
+
+
+# In[25]:
+
+
+times = op.groupby(['user_id', 'product_id'])[['order_id']].count()
+times.columns = ['Times_Bought_N']
+times.head()
+
+
+# In[26]:
+
+
+total_orders = op.groupby('user_id')['order_number'].max().to_frame('total_orders')
+total_orders.head()
+
+
+# In[27]:
+
+
+first_order_no = op.groupby(['user_id', 'product_id'])['order_number'].min().to_frame('first_order_number')
+first_order_no  = first_order_no.reset_index()
+first_order_no.head()
+
+
+# In[28]:
+
+
+span = pd.merge(total_orders, first_order_no, on='user_id', how='right')
+span.head()
+
+
+# In[29]:
+
+
+span['Order_Range_D'] = span.total_orders - span.first_order_number + 1
+span.head()
+
+
+# In[30]:
+
+
+uxp_ratio = pd.merge(times, span, on=['user_id', 'product_id'], how='left')
+uxp_ratio.head()
+
+
+# In[31]:
+
+
+uxp_ratio['uxp_reorder_ratio'] = uxp_ratio.Times_Bought_N / uxp_ratio.Order_Range_D
+uxp_ratio.head()
+
+
+# In[32]:
+
+
+uxp_ratio = uxp_ratio.drop(['Times_Bought_N', 'total_orders', 'first_order_number', 'Order_Range_D'], axis=1)
+uxp_ratio.head()
+
+
+# In[33]:
+
+
+del [times, first_order_no, span]
+
+
+# In[34]:
+
+
+uxp = uxp.merge(uxp_ratio, on=['user_id', 'product_id'], how='left')
+
+del uxp_ratio
+uxp.head()
+
+
+# In[35]:
+
+
+uxp = uxp.merge(last_five, on=['user_id', 'product_id'], how='left')
+
+del [last_five]
+uxp.head()
+
+
+# In[36]:
+
+
+uxp = uxp.fillna(0)
+uxp.head()
+
+
+# In[37]:
+
+
+data = uxp.merge(user, on='user_id', how='left')
+data.head()
+
+
+# In[38]:
+
+
+data = data.merge(prr, on='product_id', how='left')
+data.head()
+
+
+# In[39]:
+
+
+del op, user, prr, uxp
+gc.collect()
+
+
+# In[40]:
+
+
+## First approach:
+# In two steps keep only the future orders from all customers: train & test 
+orders_future = orders[((orders.eval_set=='train') | (orders.eval_set=='test'))]
+orders_future = orders_future[ ['user_id', 'eval_set', 'order_id'] ]
+orders_future.head(10)
+
+## Second approach (if you want to test it you have to re-run the notebook):
+# In one step keep only the future orders from all customers: train & test 
+#orders_future = orders.loc[((orders.eval_set=='train') | (orders.eval_set=='test')), ['user_id', 'eval_set', 'order_id'] ]
+#orders_future.head(10)
+
+## Third approach (if you want to test it you have to re-run the notebook):
+# In one step exclude all the prior orders so to deal with the future orders from all customers
+#orders_future = orders.loc[orders.eval_set!='prior', ['user_id', 'eval_set', 'order_id'] ]
+#orders_future.head(10)
+
+
+# In[41]:
+
+
+data = data.merge(orders_future, on='user_id', how='left')
+data.head(10)
+
+
+# In[42]:
+
+
+#Keep only the customers who we know what they bought in their future order
+data_train = data[data.eval_set=='train'] #
+data_train.head()
+
+
+# In[43]:
+
+
+#Get from order_products_train all the products that the train users bought bought in their future order
+data_train = data_train.merge(order_products_train[['product_id','order_id', 'reordered']], on=['product_id','order_id'], how='left' )
+data_train.head(15)
+
+
+# In[44]:
+
+
+#Where the previous merge, left a NaN value on reordered column means that the customers they haven't bought the product. We change the value on them to 0.
+data_train['reordered'] = data_train['reordered'].fillna(0)
+data_train.head(15)
+
+
+# In[45]:
+
+
+#We set user_id and product_id as the index of the DF
+data_train = data_train.set_index(['user_id', 'product_id'])
+data_train.head(15)
+
+
+# In[46]:
+
+
+#We remove all non-predictor variables
+data_train = data_train.drop(['eval_set', 'order_id'], axis=1)
+data_train.head(15)
+
+
+# In[47]:
+
+
+data_test = data[data.eval_set=='test'] #
+data_test.head()
+
+
+# In[48]:
+
+
+#We set user_id and product_id as the index of the DF
+data_test = data_test.set_index(['user_id', 'product_id']) #
+data_test.head()
+
+
+# In[49]:
+
+
+#We remove all non-predictor variables
+data_test = data_test.drop(['eval_set','order_id'], axis=1)
+#Check if the data_test DF, has the same number of columns as the data_train DF, excluding the response variable
+data_test.head()
+
+
+# In[50]:
+
+
+# TRAIN FULL 
+###########################
+## IMPORT REQUIRED PACKAGES
+###########################
+import xgboost as xgb
+
+##########################################
+## SPLIT DF TO: X_train, y_train (axis=1)
+##########################################
+X_train, y_train = data_train.drop('reordered', axis=1), data_train.reordered
+
+########################################
+## SET BOOSTER'S PARAMETERS
+########################################
+parameters = {'eval_metric':'logloss', 
+              'max_depth':'5', 
+              'colsample_bytree':'0.4',
+              'subsample':'0.7'
+             }
+
+########################################
+## INSTANTIATE XGBClassifier()
+########################################
+xgbc = xgb.XGBClassifier(objective='binary:logistic', parameters=parameters, num_boost_round=10, tree_method = 'hist')
+
+########################################
+## TRAIN MODEL
+########################################
+model = xgbc.fit(X_train, y_train)
+
+
+
+
+# In[51]:
+
+
+model.get_xgb_params()
+
+
+# In[52]:
+
+
+
+
+# In[54]:
+
+
+# Predict values for test data with our model from chapter 5 - the results are saved as a Python array
+test_pred = model.predict(data_test).astype(int)
+test_pred[0:20] #display the first 20 predictions of the numpy array
+
+
+# In[55]:
+
+
+## OR set a custom threshold (in this problem, 0.21 yields the best prediction)
+test_pred = (model.predict_proba(data_test)[:,1] >= 0.21).astype(int)
+test_pred[0:20] #display the first 20 predictions of the numpy array
+
+
+# In[56]:
+
+
+#Save the prediction in a new column in the data_test DF
+data_test['prediction'] = test_pred
+data_test.head()
+
+
+# In[57]:
+
+
+#Reset the index
+final = data_test.reset_index()
+#Keep only the required columns to create our submission file (Chapter 6)
+final = final[['product_id', 'user_id', 'prediction']]
+
+gc.collect()
+final.head()
+
+
+# In[58]:
+
+
+orders_test = orders.loc[orders.eval_set=='test',("user_id", "order_id") ]
+orders_test.head()
+
+
+# In[59]:
+
+
+final = final.merge(orders_test, on='user_id', how='left')
+final.head()
+
+
+# In[60]:
+
+
+#remove user_id column
+final = final.drop('user_id', axis=1)
+#convert product_id as integer
+final['product_id'] = final.product_id.astype(int)
+
+#Remove all unnecessary objects
+del orders
+del orders_test
+gc.collect()
+
+final.head()
+
+
+# In[61]:
+
+
+d = dict()
+for row in final.itertuples():
+    if row.prediction== 1:
+        try:
+            d[row.order_id] += ' ' + str(row.product_id)
+        except:
+            d[row.order_id] = str(row.product_id)
+
+for order in final.order_id:
+    if order not in d:
+        d[order] = 'None'
+        
+gc.collect()
+
+#We now check how the dictionary were populated (open hidden output)
+d
+
+
+# In[62]:
+
+
+#Convert the dictionary into a DataFrame
+sub = pd.DataFrame.from_dict(d, orient='index')
+
+#Reset index
+sub.reset_index(inplace=True)
+#Set column names
+sub.columns = ['order_id', 'products']
+
+sub.head()
+
+
+# In[63]:
+
+
+#Check if sub file has 75000 predictions
+sub.shape[0]
+print(sub.shape[0]==75000)
+
+
+# In[64]:
+
+
+sub.to_csv('sub.csv', index=False)
+
