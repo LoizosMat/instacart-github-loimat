7424e0329e8df9bf0bc740c40cf986cb9cca5488
LoizosMat <65346744+LoizosMat@users.noreply.github.com>
2020-06-16 10:00:12 +0300

    11

--- instacart_loimat.py
+++ instacart_loimat.py
@@ -1157,8 +1157,9 @@ from sklearn.model_selection import GridSearchCV
 ##########################################
 X_train, y_train = data_train.drop('reordered', axis=1), data_train.reordered
 
-paramGrid = {'n_estimators':[100,150,200,250], 
-              'max_depth':[4,6,8,10]
+paramGrid = {'n_estimators':[200], 
+              'max_depth':[6],
+             'learning_rate' = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3]
              }
 
 ##############
