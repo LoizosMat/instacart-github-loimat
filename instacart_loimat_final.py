25d34ea551af012cd0c2ad16221c4a5223b2f01f
LoizosMat <65346744+LoizosMat@users.noreply.github.com>
2020-06-23 19:04:06 +0300

    333

--- instacart_loimat_final.py
+++ instacart_loimat_final.py
@@ -1266,13 +1266,13 @@ from sklearn.model_selection import GridSearchCV
 X_train, y_train = data_train.drop('reordered', axis=1), data_train.reordered
 
 
-paramGrid = {'colsample_bytree':[0.7,0.8,0.9,1]
+paramGrid = {'subsample':[0.7,0.8,0.9,1]
              }
 
 ##############
 ## INSTANTIATE XGBClassifier()###
 ########################################
-xgbc = xgb.XGBClassifier(objective='binary:logistic', eval_metric='logloss', gpu_id=0, tree_method= 'gpu_hist', subsample=0.8, learning_rate=0.07, n_estimators=1000, max_depth=3, min_child_weight=1)
+xgbc = xgb.XGBClassifier(objective='binary:logistic', eval_metric='logloss', gpu_id=0, tree_method= 'gpu_hist', learning_rate=0.07, n_estimators=1000, max_depth=3, min_child_weight=1, colsample_bytree=0.9)
 
 ##############################################
 ## DEFINE HOW TO TRAIN THE DIFFERENT MODELS
